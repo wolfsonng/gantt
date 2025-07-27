2064d0b97cef59c6ac7eebed2624fc64e78224ba
Wolfsonng <98197093+wolfsonng@users.noreply.github.com>
2025-07-27 08:22:01 -0500

    Configure Vite to parse JSX in .js files

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  esbuild: {
+    loader: 'jsx',
+    include: /src\/.*\.js$/,
+  },
   server: {
     port: 3000
   }
